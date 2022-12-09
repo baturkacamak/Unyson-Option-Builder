92b17b9df2b85d408e5ac292b2c3a65768e61fba
baturkacamak <batisonline@gmail.com>
2022-12-09 18:53:47 +0100

    refactored code and added comments

--- inc/Builder.php
+++ inc/Builder.php
@@ -4,41 +4,64 @@ namespace UnysonOptionsBuilder;
 
 use UnysonOptionsBuilder\Core\Option;
 
+/**
+ * The Builder class is responsible for managing a collection of options.
+ *
+ * It allows users to add options to the builder using the `addOption()` method,
+ * and to retrieve the added options using the `getOptions()` method. The options
+ * are returned in a structured array format that can be used in WordPress and
+ * Unyson FW configuration files, or in an IDE for easier development.
+ */
 class Builder
 {
-    protected array $options = [];
+    // The addedOptions property is an array that will store the options added to the builder
+    protected array $addedOptions = [];
 
     /**
-     * @param Option $option
+     * The addOption method adds a new option to the builder.
      *
-     * @return $this|Builder
+     * @param Option $option The option to add to the builder
+     *
+     * @return $this|Builder The Builder instance, to allow method chaining
      */
     public function addOption(Option $option): static
     {
-        $this->options[$option->getId()] = $option;
+        // Add the option to the addedOptions array
+        $this->addedOptions[$option->getId()] = $option;
 
+        // Return the builder instance to allow method chaining
         return $this;
     }
 
     /**
-     * @return array
+     * The getOptions method retrieves the options that have been added to the builder.
+     *
+     * @return array An array of option arrays, in a structured format that can be
+     *               used in WordPress and Unyson FW configuration files or in an IDE
      */
     public function getOptions(): array
     {
-        $data = [];
+        // Initialize the convertedOptions array that will store the options
+        $converted_options = [];
 
+        // Loop through the options in the addedOptions array
         /** @var Option $option */
-        foreach ($this->options as $option) {
+        foreach ($this->addedOptions as $option) {
+            // Retrieve the option data as an array
             $option_array = $option->getArrayCopy();
-            $option_id    = $option_array['id'];
+            // Get the option's ID
+            $option_id = $option_array['id'];
+            // Remove the ID from the option array
             unset($option_array['id']);
-            $data[] = [
+            // Add the option array to the convertedOptions array, including the option type and ID
+            $converted_options[] = [
                 $option_id => array_merge([
                     'type' => $option->getType(),
                 ], $option_array),
             ];
         }
 
-        return $data;
+        // Return the convertedOptions array
+        return $converted_options;
     }
 }

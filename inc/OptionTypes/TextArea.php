66e67dc4a2e10b0b3128ab344123410c1556e8a5
Batur Kacamak <batisonline@gmail.com>
2022-12-13 21:58:02 +0100

    Update TextArea.php

--- inc/OptionTypes/TextArea.php
+++ inc/OptionTypes/TextArea.php
@@ -4,10 +4,16 @@ namespace UnysonOptionsBuilder\OptionTypes;
 
 use UnysonOptionsBuilder\Core\OptionAbstract;
 
+/**
+ * This class defines a `TextArea` option type for a configuration framework.
+ * It allows users to enter text in a multi-line text area.
+ */
 class TextArea extends OptionAbstract
 {
     /**
-     * @return string
+     * Gets the type identifier for the `TextArea` option type.
+     *
+     * @return string The type identifier for the `TextArea` option type.
      */
     public function getType(): string
     {

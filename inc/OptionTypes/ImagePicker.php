5d73e5e95f182735642a893375da73cd21621a3f
Batur Kacamak <batisonline@gmail.com>
2022-12-13 21:38:08 +0100

    Update ImagePicker.php

--- inc/OptionTypes/ImagePicker.php
+++ inc/OptionTypes/ImagePicker.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace UnysonOptionsBuilder\OptionTypes;
 
 use Exception;
@@ -7,23 +5,35 @@ use UnysonOptionsBuilder\Core\OptionAbstract;
 use UnysonOptionsBuilder\Core\ValidatedOption;
 
 /**
- *
+ * This class defines an `ImagePicker` option type for a configuration framework.
+ * It allows users to choose an image from a predefined palette of images.
  */
 class ImagePicker extends OptionAbstract
 {
+    /**
+     * An instance of the `ValidatedOption` class, which is used to validate the
+     * selected image before it is set as the option value.
+     *
+     * @var ValidatedOption
+     */
     protected ValidatedOption $validator;
 
+    /**
+     * The constructor method initializes the `ImagePicker` instance by creating a
+     * new `ValidatedOption` instance and storing it in the `$validator` property.
+     */
     public function __construct()
     {
         $this->validator = new ValidatedOption();
     }
 
     /**
-     * @param array $value
+     * Sets the selected image for the `ImagePicker` instance.
      *
-     * @throws Exception
+     * @param array $value An array representing the selected image.
      *
-     * @return ImagePicker
+     * @return ImagePicker The `ImagePicker` instance with the updated image value.
+     * @throws Exception If the provided value is not in the correct format.
      */
     public function withValue($value): ImagePicker
     {
@@ -44,3 +54,5 @@ class ImagePicker extends OptionAbstract
         return 'image-picker';
     }
 }
+
+

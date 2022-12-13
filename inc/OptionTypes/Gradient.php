357f57f05d2004a3dc2a21b02077a6aa4b933e95
Batur Kacamak <batisonline@gmail.com>
2022-12-13 21:31:52 +0100

    Update Gradient.php

--- inc/OptionTypes/Gradient.php
+++ inc/OptionTypes/Gradient.php
@@ -7,23 +7,35 @@ use UnysonOptionsBuilder\Core\OptionAbstract;
 use UnysonOptionsBuilder\Core\ValidatedOption;
 
 /**
- *
+ * This class defines a `Gradient` option type for a configuration framework.
+ * It allows users to choose a gradient from a predefined palette of gradients.
  */
 class Gradient extends OptionAbstract
 {
+    /**
+     * An instance of the `ValidatedOption` class, which is used to validate the
+     * selected gradient before it is set as the option value.
+     *
+     * @var ValidatedOption
+     */
     protected ValidatedOption $validator;
 
+    /**
+     * The constructor method initializes the `Gradient` instance by creating a
+     * new `ValidatedOption` instance and storing it in the `$validator` property.
+     */
     public function __construct()
     {
         $this->validator = new ValidatedOption();
     }
 
     /**
-     * @param array $value
+     * Sets the selected gradient for the `Gradient` instance.
      *
-     * @return Gradient
-     * @throws Exception
+     * @param array $value An array representing the selected gradient.
      *
+     * @return Gradient The `Gradient` instance with the updated gradient value.
+     * @throws Exception If the provided value is not in the correct format.
      */
     public function withValue($value): Gradient
     {
@@ -37,7 +49,9 @@ class Gradient extends OptionAbstract
     }
 
     /**
-     * @return string
+     * Gets the type identifier for the `Gradient` option type.
+     *
+     * @return string The type identifier for the `Gradient` option type.
      */
     public function getType(): string
     {

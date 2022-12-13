80ac13ad0023e192d6267a816da7c6863f274406
Batur Kacamak <batisonline@gmail.com>
2022-12-13 21:29:28 +0100

    added code comments

--- inc/OptionTypes/ColorPicker.php
+++ inc/OptionTypes/ColorPicker.php
@@ -5,21 +5,24 @@ namespace UnysonOptionsBuilder\OptionTypes;
 use UnysonOptionsBuilder\Core\OptionAbstract;
 
 /**
- *
+ * This class defines a `ColorPicker` option type for a configuration framework.
+ * It allows users to choose a color from a predefined palette of colors.
  */
 class ColorPicker extends OptionAbstract
 {
     /**
-     * array( '#ba4e4e', '#0ce9ed', '#941940' )
+     * An array of hexadecimal color codes representing the available color options for the `ColorPicker` instance.
      *
      * @var array
      */
     protected array $palettes;
 
     /**
-     * @param array $palettes
+     * Sets the available color options for the `ColorPicker` instance.
      *
-     * @return ColorPicker
+     * @param array $palettes An array of hexadecimal color codes
+     *
+     * @return ColorPicker The `ColorPicker` instance with the updated color options
      */
     public function withPalettes(array $palettes): ColorPicker
     {
@@ -29,7 +32,9 @@ class ColorPicker extends OptionAbstract
     }
 
     /**
-     * @return string
+     * Gets the type identifier for the `ColorPicker` option type.
+     *
+     * @return string The type identifier for the `ColorPicker` option type
      */
     public function getType(): string
     {

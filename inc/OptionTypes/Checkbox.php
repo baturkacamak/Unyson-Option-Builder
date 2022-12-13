2edf71d2475b3b01163b9ac481530971af7340ed
baturkacamak <batisonline@gmail.com>
2022-12-13 10:38:20 +0100

    added code comments

--- inc/OptionTypes/Checkbox.php
+++ inc/OptionTypes/Checkbox.php
@@ -4,26 +4,46 @@ namespace UnysonOptionsBuilder\OptionTypes;
 
 use UnysonOptionsBuilder\Core\OptionAbstract;
 
+/**
+ * The Checkbox class is a concrete implementation of the Option interface
+ * for checkbox options. It provides methods for setting and retrieving
+ * the label and value of a checkbox option, as well as its type.
+ */
 class Checkbox extends OptionAbstract
 {
-    protected string $text = '';
     /**
-     * @param string $text
+     * The $text property is a string that will store the text of the checkbox
+     * option, which will be displayed to users next to the checkbox.
      *
-     * @return Checkbox
+     * @var string
+     */
+    protected string $text = '';
+
+    /**
+     * The withText() method sets the text of the checkbox option, which will
+     * be displayed to users next to the checkbox.
+     *
+     * @param string $text The text of the checkbox option.
+     *
+     * @return Checkbox The current object, for method chaining.
      */
     public function withText(string $text): Checkbox
     {
         $this->text = $text;
 
+        // Return the current object, for method chaining.
         return $this;
     }
 
     /**
-     * @return string
+     * The getType() method returns the string "checkbox" to indicate that
+     * this is a checkbox option.
+     *
+     * @return string The option type.
      */
     public function getType(): string
     {
+        // Return the string "checkbox".
         return 'checkbox';
     }
 }
--- inc/OptionTypes/Checkboxes.php
+++ inc/OptionTypes/Checkboxes.php
@@ -7,37 +7,67 @@ use UnysonOptionsBuilder\Core\OptionAbstract;
 use UnysonOptionsBuilder\Core\ValidatedOption;
 
 /**
- *
- */
+* The Checkboxes class is a concrete implementation of the Option interface
+* for checkbox group options. It provides methods for setting and retrieving
+* the choices, value, and type of a checkbox group option.
+*/
 class Checkboxes extends OptionAbstract
 {
-
+    /**
+     * The $validator property is an instance of the ValidatedOption class,
+     * which will be used to validate the value of the checkbox group option.
+     *
+     * @var ValidatedOption
+     */
     protected ValidatedOption $validator;
 
     /**
+     * The $choices property is an array that will store the choices of the
+     * checkbox group option, which will be displayed to users as a list of
+     * checkboxes.
+     *
      * @var array
      */
     protected array $choices = [];
 
     /**
+     * The $inline property is a boolean flag that indicates whether the
+     * checkboxes in the checkbox group should be displayed inline or not.
+     *
      * @var bool
      */
     protected bool $inline = false;
 
     /**
+     * The $requiredProperties property is an array of strings that specifies
+     * the names of the required properties of the checkbox group option.
+     * These properties will be checked by the `hasRequiredFields()` method
+     * to ensure that the checkbox group option has all of the required
+     * properties before it is converted to an option array.
+     *
      * @var array|string[]
      */
     protected array $requiredProperties = ['choices'];
 
+    /**
+     * The __construct() method initializes the checkbox group option object
+     * by initializing the `$validator` property with a new instance of the
+     * `ValidatedOption` class.
+     */
     public function __construct()
     {
         $this->validator = new ValidatedOption();
     }
 
     /**
-     * @param bool $inline
+     * The withInline() method sets the value of the $inline property, which
+     * indicates whether the checkboxes in the checkbox group should be displayed
+     * inline or not.
      *
-     * @return Checkboxes
+     * @param bool $inline A boolean value that indicates whether the checkboxes
+     *                     should be displayed inline or not.
+     *
+     * @return Checkboxes The current object, for method chaining.
      */
     public function withInline(bool $inline): Checkboxes
     {
@@ -47,9 +77,12 @@ class Checkboxes extends OptionAbstract
     }
 
     /**
-     * @param array $choices
+     * The withChoices() method sets the choices of the checkbox group option,
+     * which will be displayed to users as a list of checkboxes.
      *
-     * @return Checkboxes
+     * @param array $choices An array of choices for the checkbox group option.
+     *
+     * @return Checkboxes The current object, for method chaining.
      */
     public function withChoices(array $choices): Checkboxes
     {
@@ -59,10 +92,14 @@ class Checkboxes extends OptionAbstract
     }
 
     /**
-     * @param array $value
+     * The withValue() method sets the value of * the checkbox group option and validates it using the `$validator` object.
+     * If the value is not valid, an Exception is thrown.
      *
-     * @return Checkboxes
-     * @throws Exception
+     * @param array $value The value of the checkbox group option.
+     *
+     * @return Checkboxes The current object, for method chaining.
+     *
+     * @throws Exception If the value is not valid.
      */
     public function withValue($value): Checkboxes
     {
@@ -76,10 +113,14 @@ class Checkboxes extends OptionAbstract
     }
 
     /**
-     * @return string
+     * The getType() method returns the type of the checkbox group option,
+     * which is "checkboxes".
+     *
+     * @return string The type of the checkbox group option.
      */
     public function getType(): string
     {
         return 'checkboxes';
     }
 }
+

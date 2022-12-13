237117248f1894513cda49f96114a5155d84601c
Batur Kacamak <batisonline@gmail.com>
2022-12-13 21:56:03 +0100

    Update Select.php

--- inc/OptionTypes/Select.php
+++ inc/OptionTypes/Select.php
@@ -5,34 +5,46 @@ namespace UnysonOptionsBuilder\OptionTypes;
 use UnysonOptionsBuilder\Core\OptionAbstract;
 
 /**
- *
+ * This class defines a `Select` option type for a configuration framework.
+ * It allows users to choose a value from a predefined list of options.
  */
 class Select extends OptionAbstract
 {
     /**
+     * A boolean flag indicating whether the selected value should be validated
+     * before being set as the option value.
+     *
      * @var bool
      */
     protected bool $noValidate = false;
 
     /**
+     * An array of available options for the `Select` instance.
+     *
      * @var array
      */
     protected array $choices = [];
 
     /**
+     * A boolean flag indicating whether the select options should be displayed inline.
+     *
      * @var bool
      */
     protected bool $inline = false;
 
     /**
+     * An array containing the names of the required properties for the `Select` option type.
+     *
      * @var array|string[]
      */
     protected array $requiredProperties = ['choices'];
 
     /**
-     * @param bool $noValidate
+     * Sets whether the selected value should be validated before being set as the option value.
      *
-     * @return Select
+     * @param bool $noValidate A boolean flag indicating whether the selected value should be validated.
+     *
+     * @return Select The `Select` instance with the updated value validation flag.
      */
     public function withNoValidate(bool $noValidate): Select
     {
@@ -73,3 +85,5 @@ class Select extends OptionAbstract
         return 'select';
     }
 }
+
+
